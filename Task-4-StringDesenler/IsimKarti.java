96f792847b5a070733db1651bcb17042862308ca
Hasan Kara <hasankara0870@gmail.com>
2025-10-23 13:01:36 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+public class İsimKarti {
+    public static void main(String[] args){
+        String ad = "HASAN";
+        String soyad = "KARA";
+        String ogrno = "250541015";
+        String bolum = "YAZILIM MÜHENDİSLİĞİ";
+        System.out.println("===================");
+        System.out.println(" ÖĞRENCİ BİLGİLERİ  ");
+        System.out.println("===================");
+        System.out.println("AD : " + ad);
+        System.out.println("SOYAD : " + soyad);
+        System.out.println("NO : " + ogrno);
+        System.out.println("BÖLÜM : " + bolum);
+        System.out.println("===================");
+    }
+}
