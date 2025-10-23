393ff700c39816ebd2e35d7a0b2416ff6f3ddb9e
Hasan Kara <hasankara0870@gmail.com>
2025-10-23 12:12:52 +0300

    DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,8 @@
+public class Dikdörtgen {
+    public static void main(String[] args){
+        double en = 4.5;
+        double boy = 7.9;
+        System.out.println("Alan : " + en*boy);
+        System.out.println("Çevre : " + 2*(en+boy));
+    }
+}
