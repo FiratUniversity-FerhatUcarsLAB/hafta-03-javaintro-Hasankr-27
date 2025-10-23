60484c2a051d0f6f65b5df484348a8d3c14dc66a
Hasan Kara <hasankara0870@gmail.com>
2025-10-23 15:02:16 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,18 @@
+public class DönüşümTablosu {
+    public static void main(String[] args){
+        int milkm = 1609 ;
+        int mil1 = 1*1609;
+        int mil2 = 5*1609;
+        int mil3 = 10*1609;
+        int mil4 = 20*1609;
+        int mil5 = 50*1609;
+        System.out.println("--------------");
+        System.out.println("Mil   ->   Km   ");
+        System.out.println("--------------");
+        System.out.println(1  +  "     ->   " + mil1);
+        System.out.println(5  +  "    ->    " + mil2);
+        System.out.println(10 +  "   ->    " + mil3);
+        System.out.println(20 +  "   ->    " + mil4);
+        System.out.println(50 +  "   ->    " + mil5);
+    }
+}
