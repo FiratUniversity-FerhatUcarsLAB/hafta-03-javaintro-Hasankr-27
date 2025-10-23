49bf2aeb9be597936aa57103b6f35fb086352984
Hasan Kara <hasankara0870@gmail.com>
2025-10-23 12:12:23 +0300

    DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,9 @@
+public class Daire {
+    public static void main(String[] args){
+        double pi=3.14159;
+        double r = 5.5;
+        System.out.println("Yarıçap : " + r);
+        System.out.println("Alan : " + pi*r*r);
+        System.out.println("Çevre : " + 2*pi*r);
+    }
+}
