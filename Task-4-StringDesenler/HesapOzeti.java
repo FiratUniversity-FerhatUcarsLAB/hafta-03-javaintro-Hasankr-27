05c9f9b3df1a40cefcb1d5d661838639a3462986
Hasan Kara <hasankara0870@gmail.com>
2025-10-23 13:00:44 +0300

    HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,17 @@
+public class HesapÖzeti {
+    public static void main(String[] args){
+        String urun = "Hoşbeş";
+        int miktar = 1 ;
+        double fiyat = 45.00 ;
+        double toplam = miktar*fiyat;
+        System.out.println("-------------------------");
+        System.out.println("Ürün :" + urun);
+        System.out.println("-------------------------");
+        System.out.println("Miktar :" + miktar);
+        System.out.println("-------------------------");
+        System.out.println("Fiyat :" + fiyat);
+        System.out.println("-------------------------");
+        System.out.println("Ödenecek Toplam :" + toplam);
+        System.out.println("-------------------------");
+    }
+}
